61e6f78e6313010c498c248419acfa9e0c588fe6
Bruno Rafael <brunorafaeldls2016@gmail.com>
2023-05-18 01:01:17 -0300

    feat: ✨ initial setup mobile

---
+++ mobile/App.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native'
+
+export default function App() {
+  return (
+    <View className="h-screen items-center justify-center bg-zinc-900">
+      <View className="bor flex w-80 items-center rounded-xl bg-black py-10">
+        <Text className="text-5xl font-bold text-zinc-300">Hello Word!</Text>
+      </View>
+    </View>
+  )
+}
