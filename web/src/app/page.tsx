d5a5e4090a2d984282600b91d351e1d248568e74
Bruno Rafael <brunorafaeldls2016@gmail.com>
2023-05-18 01:00:28 -0300

    feat: ✨ initial setup web

---
+++ web/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-zinc-950 ">
+      <h1 className="text-6xl font-bold uppercase text-zinc-200">Hello word</h1>
+    </div>
+  )
+}
